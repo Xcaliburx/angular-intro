3ed23c9ee02fe1c6415cdf764ec7ccaafe701fce
BENNY KHARISMA <U076181@bca.co.id>
2023-05-17 13:52:38 +0700

    Initial commit

---
+++ src/app/server/server.component.ts
@@ -0,0 +1,29 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'app-server',
+    templateUrl: './server.component.html',
+    styles: [`
+        .online {
+            color: white;
+        }
+    `]
+})
+export class ServerComponent {
+    serverId = 10; //auto define data type
+    // serverId: number = 10; 
+    serverStatus = 'offline';
+    // serverStatus: string = 'offline';
+
+    constructor() {
+        this.serverStatus = Math.random() > 0.5 ? 'online' : 'offline'
+    }
+
+    getServerStatus() {
+        return this.serverStatus;
+    }
+
+    getColor() {
+        return this.serverStatus === 'online' ? 'green' : 'red'
+    }
+}
